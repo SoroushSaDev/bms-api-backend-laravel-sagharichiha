73b018a20420229e4f6815655279aa984a1fcef3
Soroush <sagharichihas@gmail.com>
2024-10-04 13:13:44 +0330

    Added Project CRUD

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,12 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProjectRequest;
+use App\Models\Project;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
-        
+        $projects = Project::paginate(10);
+        return response()->json([
+            'status' => 'success',
+            'data' => $projects,
+        ], 200);
+    }
+
+    public function store(ProjectRequest $request): JsonResponse
+    {
+        DB::beginTransaction();
+        try {
+            $project = new Project();
+            $project->save();
+            DB::commit();
+            return response()->json([
+                'status' => 'success',
+                'data' => $project,
+            ], 200);
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 'error',
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function show(Project $project): JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'data' => $project,
+        ], 200);
+    }
+
+    public function update(Project $project, ProjectRequest $request): JsonResponse
+    {
+        DB::beginTransaction();
+        try {
+            $project->name = $request->get('name');
+            $project->type = $request->has('type') ? $request->get('type') : null;
+            $project->save();
+            DB::commit();
+            return response()->json([
+                'status' => 'success',
+                'data' => $project,
+            ], 200);
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 'error',
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function destroy(Project $project): JsonResponse
+    {
+        DB::beginTransaction();
+        try {
+            $project->delete();
+            DB::commit();
+            return response()->json([
+                'status' => 'success',
+            ], 200);
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 'error',
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
     }
 }
--- database/migrations/2024_10_03_074604_create_user_profiles_table.php
+++ database/migrations/2024_10_03_074604_create_user_profiles_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,6 +16,8 @@ return new class extends Migration
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->enum('gender', ['male', 'female'])->nullable();
+            $table->date('birthday')->nullable();
+            $table->text('address')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
