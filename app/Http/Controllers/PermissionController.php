20c7e76d9b2fb7320c3d71c9a16e9d196ffa60af
Soroush Sagharichiha <sagharichihas@gmail.com>
2024-10-07 15:12:34 +0330

    Roles $ Permissions CRUD Initial
    Took 8 minutes

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Permission;
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    public function index(Request $request)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Permission $permission)
+    {
+
+    }
+
+    public function destroy(Permission $permission)
+    {
+
+    }
+}
---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    public function index(Request $request)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Role $role)
+    {
+
+    }
+
+    public function destroy(Role $role)
+    {
+
+    }
+}
