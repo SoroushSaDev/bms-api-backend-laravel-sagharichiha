aa382f00e1274fef67aee5eff8490050b5802dbb
SoroushDev <sagharichihas@gmail.com>
2024-11-24 11:12:06 +0330

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,23 +8,23 @@ use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
+        // web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         // commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            EnsureFrontendRequestsAreStateful::class,
-        ]);
+        // $middleware->api(prepend: [
+        //     EnsureFrontendRequestsAreStateful::class,
+        // ]);
 
-        $middleware->validateCsrfTokens(except: [
-            '/api',
-        ]);
+        // $middleware->validateCsrfTokens(except: [
+        //     '/api',
+        // ]);
 
-        $middleware->alias([
-            'verified' => EnsureEmailIsVerified::class,
-        ]);
+        // $middleware->alias([
+        //     'verified' => EnsureEmailIsVerified::class,
+        // ]);
 
         //
     })
