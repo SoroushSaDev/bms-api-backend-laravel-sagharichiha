6c6c7eb81ac79bf42717dfcfdda0c823df3bcd40
SoroushDev <sagharichihas@gmail.com>
2025-03-26 13:28:56 +0330

    Form Content Nullable

--- app/Http/Requests/FormRequest.php
+++ app/Http/Requests/FormRequest.php
@@ -23,9 +23,9 @@ class FormRequest extends FRequest
     public function rules(): array
     {
         return [
-            'name' => request()->method() == 'post' ? 'required|string' : 'nullable',
-            'objects' => request()->method() == 'post' ? 'required|string' : 'nullable',
-            'content' => request()->method() == 'patch' ? 'required|string' : 'nullable',
+            'name' => 'nullable',
+            'objects' => 'nullable',
+            'content' => 'nullable',
         ];
     }
 }
